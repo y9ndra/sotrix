156be30fcf6a169f9b437ffa0153a848c393d43d
Yugi <yugendhra18@gmail.com>
2026-07-12 19:11:54 +0530

    feat: initialize database and  connected project to MongoDB atlas

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,13 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 
-const app: Application = express();
 
+const app: Application = express();
 // Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 
 app.get('/',(req: Request,res : Response)=>{
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,24 @@
 import dotenv from 'dotenv';
 import app from './app';
+import {connectDB} from "./config/db";
 
 // Load environment variables
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+async function startserver(){
+  try{
+    await connectDB();
+    const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running in development mode on port ${PORT}`);
 });
+
+  }
+  catch(err){
+    console.log(err);
+    process.exit(1);
+  }
+}
+
+startserver();
